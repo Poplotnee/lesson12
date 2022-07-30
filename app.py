325847aac7aa18496dba687e6373ef9097d59697
Евгений <sremplotna@bk.ru>
2022-07-30 14:19:12 +0300

    blue

--- app.py
+++ app.py
@@ -1,30 +1,18 @@
-from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
+from flask import Flask, send_from_directory
+import logging
+
+from main.views import main_blueprint
+from loader.views import loader_blueprint
 
 POST_PATH = "posts.json"
-UPLOAD_FOLDER = "uploads/images"
+UPLOAD_FOLDER = "uploads"
 
 app = Flask(__name__)
 
+app.register_blueprint(main_blueprint)
+app.register_blueprint(loader_blueprint)
 
-@app.route("/")
-def page_index():
-    pass
-
-
-@app.route("/list")
-def page_tag():
-    pass
-
-
-@app.route("/post", methods=["GET", "POST"])
-def page_post_form():
-    pass
-
-
-@app.route("/post", methods=["POST"])
-def page_post_upload():
-    pass
+logging.basicConfig(filename="basic.log", level=logging.INFO)
 
 
 @app.route("/uploads/<path:path>")
@@ -33,4 +21,3 @@ def static_dir(path):
 
 
 app.run()
-
--- functions.py
+++ functions.py
@@ -1,1 +1,31 @@
 import json
+
+
+def load_posts_from_json():
+    with open('posts.json', 'r', encoding='utf-8') as file:
+        posts = json.load(file)
+        return posts
+
+
+def search_post(key_search):
+    posts = load_posts_from_json()
+    find_list_posts = []
+    for post in posts:
+        if key_search in post['content']:
+            find_list_posts.append(post)
+    return find_list_posts
+
+
+def add_post(pic, cont):
+    try:
+        with open('posts.json', 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        data.append({'pic': pic, 'content': cont})
+        with open('posts.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False)
+    except FileNotFoundError:
+        # Будет выполнено, если файл не найден
+        print("Файл не найден")
+    except JSONDecodeError:
+        # Будет выполнено, если файл найден, но не превращается из JSON
+        print("Файл не удается преобразовать")
